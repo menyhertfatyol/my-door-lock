4f17d282092b0cfbb49bb6dad39098120709773c
mfatyol <menyhert.fatyol@gmail.com>
2021-07-30 14:30:46 +0200

    Refactor

--- spec/helpers/doorlock_helper_spec.rb
+++ spec/helpers/doorlock_helper_spec.rb
@@ -5,19 +5,19 @@ RSpec.describe DoorlockHelper, type: :helper do
   before { helper.extend DoorlockHelper }
 
   describe '#lock_state' do
+    subject(:lock_state) { helper.lock_state(last_lock_state) }
+
     context 'when the last database entry is older than 2 minutes' do
       let(:last_lock_state) { create :doorlock, created_at: 3.minutes.ago }
 
-      it 'returns error' do
-        expect(helper.lock_state(last_lock_state)).to eq 'error'
-      end
+      it { is_expected.to eq 'error' }
     end
 
     context 'when the last database entry was within 2 minutes' do
       let(:last_lock_state) { create :doorlock, created_at: 1.minute.ago }
 
       it 'returns the last lock state' do
-        expect(helper.lock_state(last_lock_state)).to eq last_lock_state.state
+        expect(lock_state).to eq last_lock_state.state
       end
     end
   end
